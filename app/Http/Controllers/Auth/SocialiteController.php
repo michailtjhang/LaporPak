099552fbfcf2be06ac16a7a3a80afa4c480ff582
michailtjhang <michailaja@gmail.com>
2024-11-05 12:12:19 +0700

    feature: add package socialite

---
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback($provider)
+    {
+        $socialUser = Socialite::driver($provider)->user();
+
+        $registeredUser = User::where('provider_id', $socialUser->id)
+            ->where('provider_name', $provider)
+            ->first();
+
+        if ($registeredUser) {
+            Auth::login($registeredUser);
+            return redirect('dashboard');
+        } else {
+            $user = User::updateOrCreate([
+                'provider_id' => $socialUser->id,
+                'provider_name' => $provider,
+            ], [
+                'name' => $socialUser->name,
+                'email' => $socialUser->email,
+                'password' => hash('sha256', $socialUser->email),
+                'access_token' => $socialUser->token,
+                'refresh_token' => $socialUser->refreshToken,
+            ]);
+        }
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\SocialiteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/redirect', [SocialiteController::class, 'redirect'])->name('auth.socialite.redirect');
+Route::get('/auth/{provider}/callback', [SocialiteController::class, 'callback'])->name('auth.socialite.callback');
