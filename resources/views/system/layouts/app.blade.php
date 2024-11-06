42e26aee9e122329ad1c2c1506860e9f029e3c79
michailtjhang <michailaja@gmail.com>
2024-11-06 11:29:43 +0700

    refactor: change package to cdn

--- resources/views/system/layouts/app.blade.php
+++ resources/views/system/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Dashboard' }} - {{ config('app.name') }}</title>
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     @yield('css')
 </head>
 <body class="bg-gray-100 flex flex-col min-h-screen relative">
