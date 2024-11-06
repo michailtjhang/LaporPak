c5259517dfd4eb6eb1298e5225f811400ee83937
michailtjhang <michailaja@gmail.com>
2024-11-06 12:43:06 +0700

    refactor: fix add column table

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -18,14 +18,30 @@ class SocialiteController extends Controller
     {
         $socialUser = Socialite::driver($provider)->user();
 
+        // Check if the user already exists
         $registeredUser = User::where('provider_id', $socialUser->id)
             ->where('provider_name', $provider)
             ->first();
 
         if ($registeredUser) {
+            // Login the registered user
             Auth::login($registeredUser);
             return redirect()->intended('tickets');
         } else {
+            // Generate id_number for new user
+            $latestUser = User::latest()->first();
+            $kodeUser = "US";
+
+            if ($latestUser == null) {
+                $id_number = $kodeUser . "001";
+            } else {
+                $id_number = $latestUser->id_number;
+                $urutan = (int) substr($id_number, 2, 3);
+                $urutan++;
+                $id_number = $kodeUser . sprintf("%03s", $urutan);
+            }
+
+            // Create or update the user with Socialite data and generated id_number
             $user = User::updateOrCreate([
                 'provider_id' => $socialUser->id,
                 'provider_name' => $provider,
@@ -35,9 +51,11 @@ class SocialiteController extends Controller
                 'password' => hash('sha256', $socialUser->email),
                 'access_token' => $socialUser->token,
                 'refresh_token' => $socialUser->refreshToken,
+                'id_number' => $id_number, // Set id_number for the user
             ]);
         }
 
+        // Log in the newly created user
         Auth::login($user);
 
         return redirect()->intended('tickets');
